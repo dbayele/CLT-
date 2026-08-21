ed50f37072d2d9fec29f1e96509bb83aa48f93e1
David <david.ayele@outlook.com>
2026-08-21 07:57:10 -0400

    Route account and My District pages

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import ResidentPortal from './ResidentPortal'
 import './styles.css'
 
+const path=window.location.pathname.toLowerCase()
+const root=path.startsWith('/account')||path.startsWith('/my-district')?<ResidentPortal/>:<App/>
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <React.StrictMode>{root}</React.StrictMode>,
 )
